a666e95c7ec7326d9898e420fbc5c0f2155780a3
joaovictor1205 <you@example.com>
2019-08-02 12:56:56 +0000

    implementation init

--- tests/teste.js
+++ tests/teste.js
@@ -1,11 +1,17 @@
 module.exports = {
 
-    'Test Case - Form' : function (browser) {
+    '@tags': ['googleAdvancedSearch'],
+
+    'Test Case - Form Google Advanced Search' : function (browser) {
+
+        const mainQuery = 'Test';
+        const mainQueryInputSelector = 'input[name = "as_q"]';
         
         browser
-            .url('http://google.com.br')
+            .url('http://google.com/advanced_search')
             .waitForElementVisible('body', 5000)
-            .assert.title('Google');
+            .setValue(mainQueryInputSelector, mainQuery)
+            .saveScreenshot('tests_output/google.png')
 
     }
 
