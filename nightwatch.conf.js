e01536287407360dccdb32ddb85c86c4371c8382
joaovictor1205 <you@example.com>
2019-08-03 15:30:08 +0000

    bug fixed one more time :(

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -10,13 +10,6 @@ module.exports = {
   
     "test_settings" : {
       "default" : {
-
-        'screenshots':{
-          'enabled': true,
-          'on_failure': true,
-          'on_error': true,
-          'path':'tests_output/screenshots'
-        },
         "desiredCapabilities": {
           "browserName": "chrome",
           'chromeOptions': {
